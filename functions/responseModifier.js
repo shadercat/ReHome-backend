8e76c13ea2405767a7e408d3e875e67cef13fec6
shadercat <shadercatin@gmail.com>
2020-05-12 23:05:35 +0300

    - modify response;

--- functions/responseModifier.js
+++ functions/responseModifier.js
@@ -1,5 +1,5 @@
 exports.setResponseHeaders = function (req, res, next) {
-    let requestOrigin = req.headers.origin;
+    let requestOrigin = process.env.CREDENTIAL_SITE;
     res.append('Access-Control-Allow-Origin', requestOrigin);
     res.append('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
